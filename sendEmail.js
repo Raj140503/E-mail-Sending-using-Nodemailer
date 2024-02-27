38d51f29c076f1c9b554264ce1a6b0da24b8a9c8
Raj Nanasaheb Patil <146241427+Raj140503@users.noreply.github.com>
2024-02-27 22:28:12 +0530

    Add files via upload

---
+++ sendEmail.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+
+const EMAIL_USER = 'rajpatil14052003@gmail.com';
+const EMAIL_PASSWORD = 'pcrr bmzb qwop jcsh';
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: EMAIL_USER,
+        pass: EMAIL_PASSWORD
+    }
+});
+
+const mailOptions = {
+    from: 'rajpatil14052003@gmail.com',
+    to: 'rajpatil14052003@gmail.com',
+    subject: 'Halchal',
+    text: 'Hey, How are you?.'
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        console.error('Error sending email:', error);
+    } else {
+        console.log('Email sent:', info.response);
+    }
+});
